a1c7194ae56e74874335b9f1b185e6c55b361c7d
Simple Login and Register Activity <ahmedkinga60@gmail.com>
2022-06-01 17:41:16 +0200

    Recycler View Build From Scratch

--- app/src/main/java/com/shopping/myapplication/Home_Page.kt
+++ app/src/main/java/com/shopping/myapplication/Home_Page.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.shopping.myapplication.UI.ToDo
+import com.shopping.myapplication.UI.Todo_Adapter
 import com.shopping.myapplication.databinding.ActivityHomePageBinding
 
 class Home_Page : AppCompatActivity() {
@@ -14,11 +18,17 @@ class Home_Page : AppCompatActivity() {
         binding= ActivityHomePageBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
-        binding.logout.setOnClickListener(View.OnClickListener {
-            var intent= Intent(this,Login::class.java)
-            startActivity(intent)
-            finish()
-            Toast.makeText(this,"Log Out",Toast.LENGTH_SHORT).show()
-        })
+        Push_Data_To_Recycler()
+    }
+    fun Push_Data_To_Recycler(){
+        var Note_List=ArrayList<ToDo>()
+        Note_List.add(ToDo("I Go To Gym 10:30 Am",false))
+        Note_List.add(ToDo("I Go To Gym 10:30 Am",false))
+        Note_List.add(ToDo("I Go To Gym 10:30 Am",false))
+        Note_List.add(ToDo("I Go To Gym 10:30 Am",false))
+        var ToDo_Adapter=Todo_Adapter(Note_List)
+        binding.recycler.layoutManager=LinearLayoutManager(this)
+        binding.recycler.adapter=ToDo_Adapter
+
     }
 }
---
+++ app/src/main/java/com/shopping/myapplication/UI Design/ToDo.kt
@@ -0,0 +1,3 @@
+package com.shopping.myapplication.UI
+
+data class ToDo(var note:String,var isCheck:Boolean){}
